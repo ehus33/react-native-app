a7daf113ded794d00c26bf7810f1b2294fb307d7
Ethan Hu <binhu88@yahoo.com>
2024-09-08 07:53:46 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import ReportIssueScreen from './screens/ReportIssueScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Report Issue" component={ReportIssueScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
